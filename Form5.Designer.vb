74da169b8ea539ad9ad2505e724aea8040a440ee
RochaF <7661813@formacao.iefp.pt>
2023-07-07 14:22:18 +0100

    Final

---
+++ Form5.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form5
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        Label1 = New Label()
+        Panel2 = New Panel()
+        Button8 = New Button()
+        Button7 = New Button()
+        Button6 = New Button()
+        Button5 = New Button()
+        Button4 = New Button()
+        Button3 = New Button()
+        Button2 = New Button()
+        Button1 = New Button()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Label1)
+        Panel1.Location = New Point(398, 44)
+        Panel1.Margin = New Padding(2)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(343, 58)
+        Panel1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(94, 19)
+        Label1.Margin = New Padding(2, 0, 2, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(136, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Isto é uma legenda"
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Controls.Add(Button8)
+        Panel2.Controls.Add(Button7)
+        Panel2.Controls.Add(Button6)
+        Panel2.Controls.Add(Button5)
+        Panel2.Controls.Add(Button4)
+        Panel2.Controls.Add(Button3)
+        Panel2.Controls.Add(Button2)
+        Panel2.Controls.Add(Button1)
+        Panel2.Location = New Point(34, 137)
+        Panel2.Margin = New Padding(2)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(1100, 435)
+        Panel2.TabIndex = 1
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(869, 384)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(163, 31)
+        Button8.TabIndex = 7
+        Button8.Text = "Cor de fundo default"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(458, 384)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(158, 31)
+        Button7.TabIndex = 6
+        Button7.Text = "Legenda cor default"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(84, 384)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(172, 31)
+        Button6.TabIndex = 5
+        Button6.Text = "Legenda texto default"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(413, 224)
+        Button5.Margin = New Padding(2)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(260, 61)
+        Button5.TabIndex = 4
+        Button5.Text = "Escrever um numero aleatorio de 1 a 100 na legenda"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(773, 144)
+        Button4.Margin = New Padding(2)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(259, 42)
+        Button4.TabIndex = 3
+        Button4.Text = "Mudar cor de fundo do formulario"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(84, 144)
+        Button3.Margin = New Padding(2)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(237, 42)
+        Button3.TabIndex = 2
+        Button3.Text = "Mudar cor de fundo da legenda"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(773, 52)
+        Button2.Margin = New Padding(2)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(259, 44)
+        Button2.TabIndex = 1
+        Button2.Text = "Mostrar legenda"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(84, 69)
+        Button1.Margin = New Padding(2)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(237, 42)
+        Button1.TabIndex = 0
+        Button1.Text = "Esconder legenda"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form5
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1201, 583)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        Margin = New Padding(2)
+        Name = "Form5"
+        Text = "Form5"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+End Class
---
+++ Form5.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form5
+    Dim col As Double = 1
+    Dim num As Int16
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Show()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim col1, col2, col3 As Double
+        col1 = (col * 5 + 1 + num * 3) Mod 256
+        col2 = (col * 3 + col1 * 2 + 7 + num * 15) Mod 256
+        col3 = (col * 7 + col1 * 5 + col2 * 9 + 20 + num * 27) Mod 256
+        Dim cor As Color = Color.FromArgb(col1, col2, col3)
+        Label1.BackColor = cor
+        col = (col * 17 + col1 * 13 + col2 * 23 + col3 * 27 + 37 + num * 11) Mod 750
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim col1, col2, col3 As Double
+        col1 = (col * 65 + 7 + num * 13) Mod 256
+        col2 = (col * 35 + col1 * 91 + 3 + num * 5) Mod 256
+        col3 = (col * 27 + col1 * 59 + col2 * 57 + 33 + num * 43) Mod 256
+        Dim cor As Color = Color.FromArgb(col1, col2, col3)
+        Me.BackColor = cor
+        col = (col * 67 + col1 * 93 + col2 * 77 + col3 * 69 + 99 + num * 101) Mod 900
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        num = (100 * Rnd(col * num * 11 + col * 7 + num * 3) + 1)
+        col = (col * 321 + 45 + num * 543 + num*col*259) Mod 1100
+        Label1.Text = num
+
+    End Sub
+
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Label1.Text = "Isto é uma legenda"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Label1.BackColor = DefaultBackColor
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Me.BackColor = DefaultBackColor
+    End Sub
+End Class
